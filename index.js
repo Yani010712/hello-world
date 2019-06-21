8ec6f5a019e8f29153f94dc9fa018aad8c2fb1ad
Yani Rodriguez <yani010712@gmail.com>
2019-06-21 12:20:08 -0400

    updated index file

--- index.js
+++ index.js
@@ -14,15 +14,3 @@ app.use(express.static(__dirname + '/public'));
 app.get('/', (request, response) => {
   response.sendFile(__dirname + '/public/index.html');
 });
-
-app.get('/about', (request, response) => {
-  response.sendFile(__dirname + '/public/index2.html');
-});
-
-app.get('/projects', (request, response) => {
-  response.sendFile(__dirname + '/public/projects.html');
-});
-
-app.get('/contact', (request, response) => {
-  response.sendFile(__dirname + '/public/contact.html');
-});
